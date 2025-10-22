25341316ab5a103ca65fd381512b454e8c7b521e
aydengibon-droid <aydengibon@gmail.com>
2025-10-22 14:54:36 -0400

    fixed commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import UserData from "./UserData"
 import './App.css'
 
 function App() {
@@ -24,29 +23,30 @@ function App() {
   return (
     <>
     {/* First Card */}
+    {/* First Card */}
       <div className="flex flex-col justify-center items-center h-screen w-screen gap-10">
       <div className="w-[400px]">
-      <div className="bg-cyan-600 text-white p-5 outline outline-cyan-700 rounnded-t-lg">
+      <div className="bg-cyan-600 text-white p-4 outline outline-cyan-700 rounded-t-lg">
         <h1 className="text-lg text-cyan-100">
-          {quotes[0].bio}
+          {data[0].bio}
         </h1>
       </div>
-      <div className="bg-stone-200 text center p-4 outline outline-stone-400 rounded-b-lg">
-        <h2 className="text-2xl text-cyan-700">{quotes[0].name}</h2>
-        <h3 className="text-stone-400">{quotes[0].title}</h3>
+      <div className="bg-stone-200 text-center p-4 outline outline-stone-400 rounded-b-lg">
+        <h2 className="text-3xl text-cyan-700">{data[0].name}</h2>
+        <h3 className="text-stone-400 p-2">{data[0].title}</h3>
       </div>
     </div>
 
     {/* Second Card */}
-      <div className="w-[800px] flex">
+    <div className="w-[790px] flex">
       <div className="flex justify-between mb-4 rounded-r-full">
-      <div className="p-2 bg-blue-500 w-80 rounded-l-xl outline outline-blue-500">
-      <h1 className="text-4xl text-white p-5 w-100">{quotes[1].name}</h1>
-      <h2 className="text-xl text-gray-400 pl-4">{quotes[1].title}</h2>
+      <div className="p-3 bg-blue-500 w-80 rounded-l-xl outline outline-blue-500">
+      <h1 className="text-4xl text-white p-5 w-100">{data[1].name}</h1>
+      <h2 className="text-xl text-gray-400 pl-4">{data[1].title}</h2>
       </div>
       <div className="flex items-center justify-center rounded-r-full outline outline-gray-500">
       <h1 className="text-stone-950 text-xl text-justify text-left pl-5">
-        {quotes[1].bio}
+        {data[1].bio}
       </h1>
       </div>
       </div>
@@ -54,21 +54,22 @@ function App() {
   
     {/* Third Card */}
     <div className="w-[600px]">
-    <div className="bg-rose-200 w-[200px] text-center p-4 outline-2 outline-red-400 rounded-b-lg">
+    <div className="bg-rose-400 w-[620px] text-center text-white p-5 outline-2 outline-rose-700
+     rounded-t-lg">
       <h1 className="text-lg text-white-100">
-        {quotes[2].bio}
+        {data[2].bio}
       </h1>
     </div>
-    <div className="bg-red-500 w-[200px] text-center p-4 outline-2 outline-red-400 rounded-b-lg">
-      <h2 className="text-xl text-rose-700 text-justify">{quotes[2].name}</h2>
-      <h3 className="text-rose-400 text-xl test-justify">{quotes[2].title}</h3>
+    <div className="bg-red-100 w-[200px] text-center p-4 outline-2 outline-red-400 rounded-b-lg">
+      <h2 className="text-xl text-rose-700 text-justify">{data[2].name}</h2>
+      <h3 className="text-rose-400 text-xl test-justify">{data[2].title}</h3>
     </div>
     </div>
   </div>
 
 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/UserData.tsx
+++ src/UserData.tsx
@@ -1,11 +1,26 @@
-const UserData = ({quotes}) => {
+import React from 'react';
+
+interface Quotes {
+  name: string;
+  title: string;
+  bio: string;
+  sqNum: number;
+}
+
+interface UserDataProps {
+  quotes: Quotes;
+}
+
+const UserData: React.FC<UserDataProps> = ({ quotes }) => {
   return (
     <div className="mb-6">
-    	  // access object keys with the dot operator
-          <p>Name: {quotes.name}</p>
-          <p>Number: {quotes.title}</p>
-          <p>Country: {quotes.bio}</p>
-          <p>Square of Number: {quotes.sqNum}</p>
-        </div>
-  )
-}
+      {/* access object keys with the dot operator */}
+      <p>Name: {quotes.name}</p>
+      <p>Number: {quotes.title}</p>
+      <p>Country: {quotes.bio}</p>
+      <p>Square of Number: {quotes.sqNum}</p>
+    </div>
+  );
+};
+
+export default UserData;
